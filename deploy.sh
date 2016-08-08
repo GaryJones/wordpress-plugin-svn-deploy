c4a850cb3d2ed7de2ab308569a201bb5e86ec9fa
Andy Fragen <andy@thefragens.com>
2016-08-08 16:55:03 -0700

    add .gitattributes to svn:ignore

--- deploy.sh
+++ deploy.sh
@@ -134,7 +134,8 @@ CHANGELOG.md
 Thumbs.db
 .github/*
 .git
-.gitignore" "$SVNPATH/trunk/"
+.gitignore
+.gitattributes" "$SVNPATH/trunk/"
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
